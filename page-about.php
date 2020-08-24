000f4261042eed472c9a38ffe8165fadd79767e9
Sanish <sanish_shr1995@hotmail.com>
2020-08-24 12:02:44 +0545

    About page completed

--- page-about.php
+++ page-about.php
@@ -1,3 +1,61 @@
 <?php get_header(); ?>
-hello
+<div class="left bg-lightblue py-5">
+    <div class="container">
+        <div class="row all-text-white py-3">
+            <div class="col-md-12 align-self-center">
+                <h1 class="py-3">About Mundana</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<section>
+    <div class="container h-100 my-5">
+        <div class="row">
+            <div class="col-md-12 col-lg-6 align-self-center ">
+                <div class="title text-left">
+                    <h3>We are a full service <span class="text-primary">Creative agency</span> that makes good ideas come to life.</h3>
+                    <p class="mb-0">We are an insight and behaviour driven creative marketing agency. A Full package Digital Creative Agency Specializing in: Video Production, Web Design, Branding, Brand Strategy, Content Marketing and Aerial Cinematography. </p>
+                    <ol class="py-3">
+                        <li><a href="#"> Creative Ideas</a></li>
+                        <li><a href="#"> Super Responsive for all devices</a></li>
+                        <li><a href="#"> Extensive documentation</a></li>
+                        <li><a href="#"> Excellent features</a></li>
+                    </ol>
+                </div>
+            </div>
+            <div class="col-md-10 col-lg-6 mx-md-auto align-self-center ">
+                <img class="rounded" src="<?php echo get_template_directory_uri() ?>/assets/img/demo/3.jpg" alt="">
+            </div>
+        </div>
+        <!-- row end -->
+
+        <div class="row mt-4 py-3">
+            <!-- feature 1 -->
+            <div class="col-md-4">
+                <div class="feature-box h-100 icon-primary">
+                    <div class="feature-box-icon py-1"><i class="fa fa-hands-helping fa-5x"></i></div>
+                    <h3 class="feature-box-title">Unlimited Helpers</h3>
+                    <p class="feature-box-desc">We have vast collection of sed do eiusmod tempor incididunt ut labore et <strong>Dolore magna aliqua.</strong> </p>
+                </div>
+            </div>
+            <!-- feature 2 -->
+            <div class="col-md-4">
+                <div class="feature-box h-100 icon-primary">
+                    <div class="feature-box-icon py-1"><i class="fa fa-headset fa-5x"></i></div>
+                    <h3 class="feature-box-title">Friendly Support</h3>
+                    <p class="feature-box-desc">With this template you can incididunt ut labore et dolore magna aliqua ad minim veniam quis ullamco.</p>
+                </div>
+            </div>
+            <!-- feature 3 -->
+            <div class="col-md-4">
+                <div class="feature-box h-100 icon-primary">
+                    <div class="feature-box-icon py-1"><i class="fa fa-fighter-jet fa-5x"></i></div>
+                    <h3 class="feature-box-title">Instant Solutions</h3>
+                    <p class="feature-box-desc">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>$255.00</strong>dolor in reprehe. </p>
+                </div>
+            </div>
+        </div>
+        <!-- row end -->
+    </div>
+</section>
 <?php get_footer(); ?>
